636c25c6c3a020385188ab3c77ea9226a5c00d0d
Shifu Yee <20980584+ShifuYee@users.noreply.github.com>
2018-07-27 01:21:12 -0400

    feat: height balanced binary tree

--- EPI/Binary-Trees/heightBalanced.spec.js
+++ EPI/Binary-Trees/heightBalanced.spec.js
@@ -4,13 +4,37 @@ describe(`A binary tree is said to be height-balanced if for each node in the tr
   in the height of its left and right subtrees is at most one. A perfect binary tree is
   height-balanced, as is a complete binary tree. A height-balanced binary tree does not
   have to be perfect or complete.`, function () {
-  let test1
+  let test1, test2, test3
+
+  class Node {
+    constructor (val) {
+      this.value = val
+      this.left = null
+      this.right = null
+    }
+  }
 
   beforeEach(function () {
-    test1
+    test1 = new Node(1)
+    test1.left = new Node(2)
+    test1.right = new Node(3)
+    test1.right.right = new Node(4)
+    test1.right.right.right = new Node(5)
+
+    test2 = new Node(1)
+    test2.left = new Node(2)
+    test2.right = new Node(3)
+
+    test3 = new Node(1)
+    test3.left = new Node(2)
+    test3.right = new Node(3)
+    test3.left.right = new Node(4)
+
   })
 
   it('it checks to see if the tree is height-balanced', function () {
-    expect(heightBalanced()).toEqual()
+    expect(heightBalanced(test1)).toEqual(false)
+    expect(heightBalanced(test2)).toEqual(true)
+    expect(heightBalanced(test3)).toEqual(true)
   })
 })
--- EPI/README.md
+++ EPI/README.md
@@ -24,6 +24,7 @@
     * [5.7: Compute binary tree nodes in order of increasing depth](./Stacks-Queues/increasingDepth.js)
     * [5.8: Implement a circular queue](./Stacks-Queues/circularQueue.js)
 6. Binary Trees
+    * [6.1: Test if a binary tree is height-balanced](./Binary-Trees/heightBalanced.js)
     * [6.3: Compute the lowest common ancestor in a binary tree](./Binary-Trees/lowestCommonAncestor.js)
     * [6.4: Compute the LCA when nodes have parent pointers](./Binary-Trees/LCAparent.js)
 7. Heaps
